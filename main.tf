bc3beac8ccb524bf75a3ae95a4d91183749e92cb
Scott Bowe <scottb@sentania.net>
2020-06-30 11:58:25 -0500

    asdf

--- main.tf
+++ main.tf
@@ -15,28 +15,42 @@ resource "vra_blueprint" "this" {
   project_id = data.vra_project.this.id
 
   content = <<-EOT
-    formatVersion: 1
-    inputs: {}
-    resources:
-      Cloud_SecurityGroup_1:
-        type: Cloud.SecurityGroup
-        properties:
-          constraints:
-            - tag: "sg:RiskyBusiness"
-          securityGroupType: existing
-      Cloud_Machine_1:
-        type: Cloud.Machine
-        properties:
-          image: CentOS7
-          flavor: Small
-          networks:
-             constraints:
-            - tag: "dynamicNetwork:Routed"
-          customizationSpec: custSpec-CentOS7
-          tags:
-            - key: protection
-              value: bkupnoCred
-            - key: operatingSystem
-              value: centOS
+  formatVersion: 1
+  inputs: {}
+  version: 1.0
+  resources:
+    Cloud_SecurityGroup_1:
+      type: Cloud.SecurityGroup
+      properties:
+        constraints:
+          - tag: 'sg:RiskyBusiness'
+        securityGroupType: existing
+    Cloud_Machine_1:
+      type: Cloud.Machine
+      properties:
+        image: CentOS7
+        flavor: Small
+        customizationSpec: custSpec-CentOS7
+        tags:
+          - key: protection
+            value: bkupnoCred
+          - key: operatingSystem
+            value: centOS
+
   EOT
 }
+resource "vra_deployment" "this" {
+  name        = var.deployment_name
+  description = "Simple IaC Demo"
+
+  blueprint_id      = vra_blueprint.blueprint_id
+  blueprint_version = vra_blueprint.blueprint_version
+  project_id        = data.vra_project.this.id
+
+  expand_resources    = true
+  expand_last_request = true
+  timeouts {
+    create = "60m"
+    delete = "2h"
+  }
+   
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,5 @@
 insecure = false
-project_name = "Test Project 3"
+project_name = "LabSandbox"
 blueprint_name = "Terraform Blueprint"
 url = "https://vra8.lab.sentania.net/"
+deployment_name = "Advanced IaC"
