610e747c2abc4b198f75480f5fd3994a2bbda7ec
Scott Bowe <scottb@sentania.net>
2020-06-30 11:33:23 -0500

    preflight

--- main.tf
+++ main.tf
@@ -16,18 +16,34 @@ resource "vra_blueprint" "this" {
 
   content = <<-EOT
     formatVersion: 1
-    inputs:
-      image:
-        type: string
-        description: "Image"
-      flavor:
-        type: string
-        description: "Flavor"
+    inputs: {}
     resources:
-      Machine:
+      Cloud_SecurityGroup_1:
+        type: Cloud.SecurityGroup
+        properties:
+          constraints:
+            - tag: 'sg:RiskyBusiness'
+          securityGroupType: existing
+      Cloud_Machine_1:
         type: Cloud.Machine
         properties:
-          image: $${input.image}
-          flavor: $${input.flavor}
+          image: CentOS7
+          flavor: Small
+          networks:
+            - network: '${resource.Cloud_NSX_Network_1.id}'
+              securityGroups:
+                - '${resource.Cloud_SecurityGroup_1.id}'
+          customizationSpec: custSpec-CentOS7
+          tags:
+            - key: protection
+              value: bkupnoCred
+            - key: operatingSystem
+              value: centOS
+      Cloud_NSX_Network_1:
+        type: Cloud.NSX.Network
+        properties:
+          networkType: routed
+          constraints:
+            - tag: 'dynamicNetwork:Routed'
   EOT
 }
