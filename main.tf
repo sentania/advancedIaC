36a8562c17d43e3da7b82f94e8ec7e0c66e5d0eb
Scott Bowe <scottb@sentania.net>
2020-06-30 11:30:07 -0500

    update

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "vra" {
   insecure      = var.insecure
 }
 
-resource "vra_project" "this" {
+data "vra_project" "this" {
   name = var.project_name
 }
 
@@ -12,7 +12,7 @@ resource "vra_blueprint" "this" {
   name        = var.blueprint_name
   description = "Created by vRA terraform provider"
 
-  project_id = vra_project.this.id
+  project_id = data.vra_project.this.id
 
   content = <<-EOT
     formatVersion: 1
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
 insecure = false
-project_name = "Test Project 2"
+project_name = "Test Project 3"
 blueprint_name = "Terraform Blueprint"
 url = "https://vra8.lab.sentania.net/"
